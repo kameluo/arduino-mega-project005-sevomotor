d3e3aca13676762d3e24ffeb9f8a7769e172bfe9
kamel <ahmedkamel_1988@hotmail.com>
2019-03-20 20:08:36 +0100

    first commit

---
+++ arduino_mega_project005_servo/arduino_mega_project005_servo.cpp
@@ -0,0 +1,25 @@
+// Do not remove the include below
+#include "arduino_mega_project005_servo.h"
+
+#include <Arduino.h>
+#include <Servo.h>
+Servo myServo;
+int servoPin=9;
+
+//controling a servo motor
+void setup() {
+myServo.attach(servoPin);
+}
+
+void loop() {
+for (int i=0;i<=180;i=i+20){
+
+  myServo.write(i);
+  delay(1000);
+  }
+  for (int i=180;i>=0;i=i-20){
+
+  myServo.write(i);
+  delay(1000);
+  }
+}
---
+++ arduino_mega_project005_servo/spec.d
@@ -0,0 +1,2 @@
+spec.o: \
+ /Users/AhmedKamel/eclipse-workspace/eclipse-phantom/.metadata/.plugins/org.eclipse.cdt.managedbuilder.core/spec.C
